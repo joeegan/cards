16ca27c64a256cfb52f64edd64f131dace1d9ce3
joe <joe@joeegan.co.uk>
2016-02-20 11:26:18 +0000

    hitMe returns a random card from the deck

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+'use strict';
+
 function buildDeck() {
   return 'HSDC'.replace(/./g, (house) =>
     'A123456789JQK'.replace(/(\w)/g, '$1' + house)
   ).match(/.{1,2}/g);
 }
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomItem(arr) {
+  return arr[randomInt(0, arr.length - 1)];
+}
+
+class Deck {
+
+  constructor() {
+    this.cards = buildDeck();
+  }
+
+  /*
+   * @return {String} 'QH'
+   */
+  hitMe() {
+    if (!this.cards.length) {
+      throw 'Sorry, no cards left';
+    }
+    return this.cards.splice(
+      this.cards.indexOf(randomItem(this.cards)),
+      1
+    )[0];
+  }
+
+}
+module.exports = Deck;

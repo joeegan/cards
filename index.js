e0da8ca965240085c1673b8a20d2a3c9d4a6578c
joe <joe@joeegan.co.uk>
2016-02-21 16:49:10 +0000

    move shuffles outside the class

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 'use strict';
 
-function buildDeck() {
-  return 'HCDS'.replace(/./g, (house) =>
-    'A23456789TJQK'.replace(/(\w)/g, '$1' + house)
-  ).match(/.{1,2}/g);
-}
-
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomItem(arr) {
-  return arr[randomInt(0, arr.length - 1)];
-}
-
-class Deck {
+module.exports = class Deck {
 
   constructor() {
     this.cards = buildDeck();
@@ -27,31 +13,51 @@ class Deck {
     return this.cards.pop();
   }
 
-  overhandShuffle() {
-    for (var i = 0; i < randomInt(5,100); i++) {
-      // Take a hand of a random amount of cards from the bottom of the deck
-      let hand = this.cards.splice(randomInt(1, 10));
-      // Take a random amount of cards from the bottom the hand
-      let remainderHand = hand.splice(randomInt(1, 10));
-      // Put what remains of the hand at the top of the deck
-      this.cards = hand.concat(this.cards);
-      // Put the other hand on top
-      this.cards = remainderHand.concat(this.cards);
-    }
-    return this.cards;
+  riffleShuffle() {
+    return this.cards = riffleShuffle(this.cards);
   }
 
-  riffleShuffle() {
-    const deckLength = this.cards.length;
-    const secondHalf = this.cards.splice(deckLength/2);
-    const firstHalf = this.cards.slice();
-    let newArray = [];
-    for (let j = 0; j < deckLength; j++) {
-      newArray.push((j % 2 == 0) ? firstHalf.shift() : secondHalf.shift());
-    }
-    this.cards = newArray;
-    return this.cards;
+  overhandShuffle() {
+    return this.cards = overhandShuffle(this.cards);
   }
 
 }
-module.exports = Deck;
+
+function buildDeck() {
+  return '♣♦♥♠'.replace(/./g, (house) =>
+    'A23456789TJQK'.replace(/(\w)/g, '$1' + house)
+  ).match(/.{1,2}/g);
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomItem(arr) {
+  return arr[randomInt(0, arr.length - 1)];
+}
+
+function overhandShuffle(deck) {
+  for (var i = 0; i < 1000; i++) {
+    // Take a hand of a random amount of cards from the bottom of the deck
+    let hand = deck.splice(randomInt(1, 10));
+    // Take a random amount of cards from the bottom the hand
+    let remainderHand = hand.splice(randomInt(1, 10));
+    // Put what remains of the hand at the top of the deck
+    deck = hand.concat(deck);
+    // Put the other hand on top
+    deck = remainderHand.concat(deck);
+  }
+  return deck;
+}
+
+function riffleShuffle(deck) {
+  const deckLength = deck.length;
+  const secondHalf = deck.splice(deck.length/2);
+  const firstHalf = deck.slice();
+  const newDeck = [];
+  for (let i = 0; i < deckLength; i++) {
+    newDeck.push((i % 2 == 0) ? firstHalf.shift() : secondHalf.shift());
+  }
+  return newDeck;
+}

8c047097550ee21015016b228bd402a8f0772bbc
joe <joe@joeegan.co.uk>
2016-03-02 22:13:48 +0000

    docs: percentage util

--- src/percentage.js
+++ src/percentage.js
@@ -1,4 +1,6 @@
 /*
- * @return The value as a percentage of the total
+* @param {Number} total
+ * @param {Number} value
+ * @return The value as a percentage of the total to 2 decimal places.
  */
 module.exports = (total, value) => +(value / total * 100).toFixed(2);
